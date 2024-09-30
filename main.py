fe4c3bcac35fd0fdd0ca100b811a9392af4bda30
Marius Nicolae <Marius.A.Nicolae@outlook.com>
2024-09-30 03:31:13 +0300

    minor update

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import requests
 
 def main():
     city = input("Enter City:")
+    city_already_inputed = 'tokyo'
     api_key = "48ebea5a0b5c48637b05be3e27ea0d91"
     # url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid={}&units=metric'.format(city, api_key)
     url2 = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
