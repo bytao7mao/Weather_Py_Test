45b2df1318f2a40b5188a6fc4e4d8b38f2aee6cd
Marius Nicolae <Marius.A.Nicolae@outlook.com>
2025-11-24 21:24:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import os
 API_KEY = os.environ.get("WEATHER_API_KEY")
 EMAIL_ADDRESS = os.environ.get("GMAIL_USERNAME")
 EMAIL_PASSWORD = os.environ.get("GMAIL_PASSWORD")
-gmailAppPassword = os.environ.get("gmailAppPassword") #jenkins
+GMAIL_APP_PASSWORD = os.environ.get("gmailAppPassword") #jenkins
 RECIPIENT_EMAIL = 'marius.a.nicolae@outlook.com'
 LOCATION = 'BUCHAREST'
 PORT = 465
